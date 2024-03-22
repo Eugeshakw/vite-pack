2e31b9af611339aaf19a59e0eeaf8d06cfb2df52
Eugeshakw <eugeshakw@gmail.com>
2024-03-22 20:00:33 +0200

    g

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import createSvgSpritePlugin from "vite-plugin-svg-sprite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // createSvgSpritePlugin({
+    //   symbolId: "icon-[name]-[hash]",
+    // }),
+  ],
+
   base: '/vite-pack',
 
 })
