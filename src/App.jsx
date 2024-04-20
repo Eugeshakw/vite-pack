58cc184bca71fbdd657aedb7ce5bf389492cf987
Eugeshakw <eugeshakw@gmail.com>
2024-04-20 16:43:26 +0300

    update build last

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { fetchCars } from "./redux/carsData/dataOperations";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout/Layout";
+import { Catalog } from "./page/Catalog";
+import { Favorites } from "./page/Favorites";
+import { UIModal } from './components/uikit/modal/Modal';
+import { useData } from './hooks/useData';
+import { HomePage } from "./page/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {isOpenModal} = useData();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-          
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites/>}/>
+        </Route>
+      </Routes>
+      {isOpenModal ? <UIModal /> : null}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { GlobalStyle } from "./globalStyles/globalStyles.styled.js";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-          <App />
-      
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/vite-pack">
+          <GlobalStyle />
+          <App/>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
-)
+);
